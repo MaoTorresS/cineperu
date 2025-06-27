6dfcb2ed1e3ab3189b9c48c5084f0ce99f2c5efc
MaoTorresS <a1612775@idat.pe>
2025-06-27 18:50:50 -0500

    Subir todos los últimos cambios, incluyendo eliminaciones y nuevas carpetas

--- src/controladores/usuario.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import { prisma } from '../prisma/client';
-
-export const obtenerUsuarios = async (_req: Request, res: Response) => {
-  const usuarios = await prisma.usuario.findMany();
-  res.json(usuarios);
-};
--- src/rutas/auth.ruta.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { registro, login } from '../controladores/auth.controller';
-
-const router = Router();
-
-router.post('/registro', registro);
-router.post('/login', login); // ✅ esta es la que usaremos ahora
-
-export default router;
