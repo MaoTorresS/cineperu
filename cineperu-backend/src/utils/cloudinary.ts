ee102af4620320b67e6c1761bc45c1ff60a55394
MaoTorresS <a1612775@idat.pe>
2025-08-11 13:46:57 -0500

    actualiza ruta de imagenes cloudinary 4

--- cineperu-backend/src/utils/cloudinary.ts
+++ cineperu-backend/src/utils/cloudinary.ts
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 cloudinary.config({
   cloud_name: 'dkykizcnt',
   api_key: '988746616953259',
-  api_secret: 'secret',
+  api_secret: '4TrEudcNiEK8yvtlGzSm2tyZkAg',
 });
 
 export default cloudinary;
